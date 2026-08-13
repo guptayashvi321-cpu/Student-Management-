bde01f707fb20b83c74bb45d6de04c32250b8593
guptayashvi321-cpu <guptayashvi321@gmail.com>
2026-08-13 08:10:51 +0530

    Add Student Information

---
+++ student.py
@@ -0,0 +1,12 @@
+students = [
+    {
+        "name": "sakshi",
+        "enrollment": "101",
+        "marks": [80, 75, 90]
+    },
+    {
+        "name": "Yashvi",
+        "enrollment": "102",
+        "marks": [85, 90, 88]
+    }
+]
